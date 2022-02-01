2f0c46e2a3e36c89202b1e9afbe79a88a4f49426
Tom Nehil <tnehil@minnpost.com>
2022-02-01 06:24:55 -0600

    Adds data table to Contest display

--- src/Contest.svelte
+++ src/Contest.svelte
@@ -1,6 +1,6 @@
 <script>
-  export let contestData;
 
+  export let contestData;
 	import highcharts from './highcharts';
 
   let candidates = []
@@ -11,7 +11,7 @@
 
   let styleGeneral = {
     fontFamily: '"Open Sans", Helvetica, Arial, "Lucida Grande", sans-serif',
-    fontSize: '14px',
+    fontSize: '18px',
     fontWeight: 'normal',
     color: "#676767"
   }
@@ -26,9 +26,9 @@
       text: null
     },
     yAxis: {
-        title: {
-            text: 'Dollars'
-        }
+      title: {
+          text: 'Dollars'
+      }
     },
     xAxis: {
       categories: candidates,
@@ -63,6 +63,47 @@
 
 </script>
 
-<p><small>Showing data as of {contestData[0].period}</small></p>
+<style>
+  .right {
+    text-align: right;
+  }
+
+  table {
+    width: 100%;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: .8em;
+    line-height: 1.5em;
+    text-align: left;
+  }
+
+  tr:nth-child(even) td {
+    background-color: #F8F8F8;
+  }
+
+  th {
+    vertical-align: bottom;
+    border-bottom: 2px solid #cccccc;
+    padding: 0.5em 0.5em 0 0.5em;
+  }
+
+  td {
+    padding: 0.5em;
+    vertical-align: middle;
+  }
+</style>
+
+<p><small>Showing data as of {contestData[0].period}.</small></p>
 
 <div class="chart" use:highcharts={config}></div>
+
+<table>
+  <thead>
+    <tr><th>Candidate</th><th>Party</th><th class="right">Total raised</th><th class="right">Cash on hand</th><th class="right">Link to filing</th></tr>
+  </thead>
+  <tbody>
+    {#each contestData as candidate}
+      <tr><td>{candidate.candidate}</td><td>{candidate.party}</td><td class="right">{parseFloat(candidate.raised).toLocaleString('en-US')}</td><td class="right">{parseFloat(candidate.onhand).toLocaleString('en-US')}</td><td>{#if candidate.link}<a href="{candidate.link}">Filing <i class="fas fa-document"></i></a>{/if}</td></tr>
+    {/each}
+  </tbody>
+
+</table>
